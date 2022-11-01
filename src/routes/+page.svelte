d67ba9f514254b10bc8483f0b83c7ae7a65333bd
ninetynin <wekandmusic@gmail.com>
2022-11-01 19:27:24 +0530

    updated assets loc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import Greet from "$lib/Greet.svelte";
 </script>
 
-<h1>Welcome to Tauri!</h1>
+<!-- <h1>Welcome to Tauri!</h1> -->
 
 <div class="row">
   <a href="https://vitejs.dev" target="_blank">
@@ -14,20 +14,23 @@
   <a href="https://kit.svelte.dev" target="_blank">
     <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
   </a>
+  <a href="https://sample.com">
+    <img src="./favicon.png" class="logo svelte" alt="COA" />
+  </a>
 </div>
 
-<p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
+<!-- <p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
 
 <div class="row">
   <Greet />
-</div>
+</div> -->
 
 <style>
   .logo.vite:hover {
     filter: drop-shadow(0 0 2em #747bff);
   }
 
-  .logo.svelte:hover {
+  /* .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00);
-  }
+  } */
 </style>
