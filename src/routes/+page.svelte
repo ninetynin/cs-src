b8261d77b7ee41e0649ff0055071ef82839d0bc0
ninetynin <wekandmusic@gmail.com>
2022-11-01 19:37:49 +0530

    bsic-struct2 done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,35 +2,54 @@
   import Greet from "$lib/Greet.svelte";
 </script>
 
-<!-- <h1>Welcome to Tauri!</h1> -->
+<h1>CS-src</h1>
 
-<div class="row">
-  <a href="https://vitejs.dev" target="_blank">
-    <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-  </a>
-  <a href="https://tauri.app" target="_blank">
-    <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-  </a>
-  <a href="https://kit.svelte.dev" target="_blank">
-    <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-  </a>
-  <a href="https://sample.com">
-    <img src="./favicon.png" class="logo svelte" alt="COA" />
-  </a>
+<div class="row" style="text-align: center">
+   <div class="COA">
+      <a href="" >
+        <img src="./yellow-folder.png" class="logo svelte" alt="COA" /> 
+      </a>
+      <p>COA</p>
+   </div>
+   <div class="DSA">
+      <a href="">
+        <img src="./yellow-folder.png" class="logo svelte" alt="COA" /> 
+      </a>
+      <p>DSA</p>
+    </div>
+    <div class="DSD">
+      <a href="">
+        <img src="./yellow-folder.png" class="logo svelte" alt="COA" /> 
+      </a>
+      <p>DSD</p>
+      </div> 
+    <div class="OOPS">
+      <a href="" >
+        <img src="./yellow-folder.png" class="logo svelte" alt="COA" /> 
+      </a>
+      <p>OOPS</p>
+      </div>
+    <div class="MATHS">
+      <a href="">
+        <img src="./yellow-folder.png" class="logo svelte" alt="COA" /> 
+      </a>
+      <p>MATHS</p>
+      </div>
 </div>
-
-<!-- <p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
-
-<div class="row">
-  <Greet />
-</div> -->
 
 <style>
   .logo.vite:hover {
     filter: drop-shadow(0 0 2em #747bff);
   }
 
-  /* .logo.svelte:hover {
+  .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00);
-  } */
+  }
+
+  .row {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+  }
 </style>
