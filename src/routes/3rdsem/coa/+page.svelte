eb97a342736381297cfd20fe0e1f92efe755d417
ninetynin <wekandmusic@gmail.com>
2022-11-02 00:35:05 +0530

    partially done

--- src/routes/3rdsem/coa/+page.svelte
+++ src/routes/3rdsem/coa/+page.svelte
@@ -1,1 +1,16 @@
 <h1>coa</h1>
+
+<div class="row" style="text-align: center">
+   <div class="latest-slides">
+      <a href="/3rdsem/coa/latest-slides" >
+        <img src="../idkgreen-folder.png" class="logo coa" alt="COA" /> 
+      </a>
+      <p>latest-slides</p>
+    </div>
+    <div class="others">
+      <a href="/3rdsem/coa/others" >
+        <img src="../yaviolet-folder.png" class="logo coa" alt="COA" /> 
+      </a>
+      <p>..</p>
+    </div>
+</div>
