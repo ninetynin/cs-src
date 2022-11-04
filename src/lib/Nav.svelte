b1734f4ed462d3b06c4885658a70e1677a884757
ninetynin <wekandmusic@gmail.com>
2022-11-04 19:49:56 +0530

    kjdehfj

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
+<div>
+  <nav>
+    <a href="{base}/"><h3 class="home">HOME</h3></a>
+    <!-- <a href="{base}/about"><h3 class="about">about</h3></a> -->
+  </nav>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,58 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+// import adapter from '@sveltejs/adapter-auto';
+// import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    // preprocess: preprocess(),
-    preprocess: preprocess[preprocess()],
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//     // Consult https://github.com/sveltejs/svelte-preprocess
+//     // for more information about preprocessors
+//     // preprocess: preprocess(),
+//     preprocess: preprocess[preprocess()],
 
+//     kit: {
+//         // adapter: adapter()
+//         adapter: adapter({
+//             pages: 'build',
+//             assets: 'build',
+//             fallback: null,
+//         }),
+//         // adapter: staticAdapter({
+//         //     pages: 'build',
+//         //     assets: 'build',
+//         //     fallback: null,
+//         // }),
+//         target: '#svelte',
+//     }
+// };
+
+// export default config;
+
+
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
+import static_adapter from '@sveltejs/adapter-static';
+
+export default {
     kit: {
-        // adapter: adapter()
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: null,
-        }),
-        // adapter: staticAdapter({
+        // adapter: adapter({
+        //     // default options are shown. On some platforms
+        //     // these options are set automatically — see below
         //     pages: 'build',
         //     assets: 'build',
         //     fallback: null,
-        // }),
-        target: '#svelte',
+        //     precompress: false,
+        //     strict: true
+        // })
+        adapter: static_adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: '/CS-SRC',
+            assets: '/CS-SRC'
+        }
+
     }
 };
-
-export default config;
