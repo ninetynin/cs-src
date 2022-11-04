279c71341704a149569b034faa8e78623ee027a7
ninetynin <wekandmusic@gmail.com>
2022-11-05 03:52:58 +0530

    test sometjing

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import {base} from '$app/paths';
-</script>
-
-<div>
-    <nav>
-        <a href="{base}/"><h3 class="home">HOME</h3></a>
-    </nav>
-</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
+
+<div>
+    <nav>
+        <a href="{base}/"><h3 class="home">HOME</h3></a>
+        <!-- <a href="{base}/about"><h3 class="about">about</h3></a> -->
+    </nav>
+</div>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
