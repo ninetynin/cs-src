bc395f972b174c84d96ebaa3c89758fca99c6aa2
ninetynin <wekandmusic@gmail.com>
2022-11-06 05:40:43 +0530

    nav path configure test

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.css";
+  import { base } from "$app/paths";
+</script>
+
+<main class="container">
+    <nav>
+        <a href="{base}/"><h3 class="home">HOME</h3></a>
+        <!-- <a href="{base}/about"><h3 class="about">about</h3></a> -->
+    </nav>
+  <slot />
+</main>
