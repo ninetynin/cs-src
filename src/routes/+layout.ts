49ffb26112acacb0e7dab6be4ce9a78e1fc1ccdf
ninetynin <wekandmusic@gmail.com>
2022-11-06 06:27:16 +0530

    idk

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,3 @@
 export const prerender = true; // This can be false if you're using a fallback (i.e. SPA mode)
-export const ssr = false;
-export const csr = true;
+// export const ssr = false;
+// export const csr = true;
--- svelte.config.js
+++ svelte.config.js
@@ -1,57 +1,83 @@
-// import adapter from '@sveltejs/adapter-auto';
-// import preprocess from 'svelte-preprocess';
+// // import adapter from '@sveltejs/adapter-auto';
+// // import preprocess from 'svelte-preprocess';
 
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-//     // Consult https://github.com/sveltejs/svelte-preprocess
-//     // for more information about preprocessors
-//     // preprocess: preprocess(),
-//     preprocess: preprocess[preprocess()],
+// // /** @type {import('@sveltejs/kit').Config} */
+// // const config = {
+// //     // Consult https://github.com/sveltejs/svelte-preprocess
+// //     // for more information about preprocessors
+// //     // preprocess: preprocess(),
+// //     preprocess: preprocess[preprocess()],
 
+// //     kit: {
+// //         // adapter: adapter()
+// //         adapter: adapter({
+// //             pages: 'build',
+// //             assets: 'build',
+// //             fallback: null,
+// //         }),
+// //         // adapter: staticAdapter({
+// //         //     pages: 'build',
+// //         //     assets: 'build',
+// //         //     fallback: null,
+// //         // }),
+// //         target: '#svelte',
+// //     }
+// // };
+
+// // export default config;
+
+
+// // svelte.config.js
+// import adapter from '@sveltejs/adapter-static';
+// import static_adapter from '@sveltejs/adapter-static';
+
+// export default {
 //     kit: {
-//         // adapter: adapter()
-//         adapter: adapter({
-//             pages: 'build',
-//             assets: 'build',
-//             fallback: null,
-//         }),
-//         // adapter: staticAdapter({
+//         // adapter: adapter({
+//         //     // default options are shown. On some platforms
+//         //     // these options are set automatically — see below
 //         //     pages: 'build',
 //         //     assets: 'build',
 //         //     fallback: null,
-//         // }),
-//         target: '#svelte',
+//         //     precompress: false,
+//         //     strict: true
+//         // })
+//         adapter: static_adapter({
+//             pages: 'build',
+//             assets: 'build',
+//             fallback: null,
+//             precompress: false,
+//             strict: true
+//         }),
+//         paths: {
+//             base: '/CS-SRC',
+//             // assets: '/CS-SRC/static'
+//         }
 //     }
 // };
 
-// export default config;
 
-
+// -- https://github.com/sveltejs/kit/blob/7d985a152d865c28352da46a63f959d881c1a094/packages/adapter-static/README.md#github-pages
 // svelte.config.js
+
+import { dev } from '$app/environment';
 import adapter from '@sveltejs/adapter-static';
-import static_adapter from '@sveltejs/adapter-static';
 
 export default {
     kit: {
-        // adapter: adapter({
-        //     // default options are shown. On some platforms
-        //     // these options are set automatically — see below
-        //     pages: 'build',
-        //     assets: 'build',
-        //     fallback: null,
-        //     precompress: false,
-        //     strict: true
-        // })
-        adapter: static_adapter({
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
             pages: 'build',
             assets: 'build',
             fallback: null,
             precompress: false,
             strict: true
-        }),
-        paths: {
-            base: '/CS-SRC',
-            // assets: '/CS-SRC/static'
-        }
-    }
+        })
+    },
+    paths: {
+        base: dev ? '' : '/CS-SRC',
+        assets: dev ? '' : '/CS-SRC/static'
+    },
+    appdir: 'internal',
 };
