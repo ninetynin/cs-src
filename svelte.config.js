b45453a380fac1118f0922452c2dcc1eb068b7bf
ninetynin <wekandmusic@gmail.com>
2022-11-06 09:52:02 +0530

    for website test starts

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,36 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { dev } from '$app/environment';
+
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+    // kit: {
+    //     adapter: adapter()
+    // }
+
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false,
+            strict: true
+        })
+    },
+    paths: {
+        base: dev ? '' : '/cs-src',
+        assets: dev ? '' : '/cs-src/static'
+    },
+    appdir: 'internal',
 };
 
 export default config;
