f7daf97aedc0e1107aff7f21bfe1b7b3573d70fc
ninetynin <wekandmusic@gmail.com>
2022-11-07 16:38:48 +0530

    fixed nav idk if it did work

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,24 @@
 </script>
 
 <main class="container">
+  <nav>
+    <a href="/" class="logo">CSE MIT MANIPAL SRC</a>
+    <ul>
+      <li><a href="/3rdsem">3rd Sem</a></li>
+        <li><a href="/3rdsem/coa">COA</a></li>
+          <li><a href="/3rdsem/coa/latest-slides">latest-slides</a></li>
+          <li><a href="/3rdsem/coa/others">others</a></li>
+        <li><a href="/3rdsem/dsa">DSA</a></li>
+        <li><a href="/3rdsem/dsd">DSD</a></li>
+        <li><a href="/3rdsem/oops">OOPS</a></li>
+        <li><a href="/3rdsem/maths">MATHS</a></li>
+      <li><a href="/4thsem">4th Sem</a></li>
+      <li><a href="/5thsem">5th Sem</a></li>
+      <li><a href="/6thsem">6th Sem</a></li>
+      <li><a href="/7thsem">7th Sem</a></li>
+      <li><a href="/8thsem">8th Sem</a></li>
+    </ul>
+  </nav>
   <slot> </slot>
 </main>
 
