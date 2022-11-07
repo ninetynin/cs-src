8133193a2b3a0d3997868294db17efee0dc9debf
ninetynin <wekandmusic@gmail.com>
2022-11-07 17:08:31 +0530

    with and wirhour base

--- src/routes/3rdsem/coa/+page.svelte
+++ src/routes/3rdsem/coa/+page.svelte
@@ -1,9 +1,12 @@
+<script>
+  import { base } from '$app/paths';
+</script>
 <h1>coa</h1>
 
 <div class="row" style="text-align: center">
    <div class="latest-slides">
-      <a href="/3rdsem/coa/latest-slides" >
-        <img src="../idkgreen-folder.png" class="logo coa" alt="COA" /> 
+      <a href="{base}/3rdsem/coa/latest-slides" >
+        <img src="{base}/idkgreen-folder.png" class="logo coa" alt="COA" /> 
       </a>
       <p>latest-slides</p>
     </div>
