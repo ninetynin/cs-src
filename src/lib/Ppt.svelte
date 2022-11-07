0d8161b1467287fd82d063cc2ba6cda296850033
ninetynin <wekandmusic@gmail.com>
2022-11-07 19:51:57 +0530

    updated ppts

---
+++ src/lib/Ppt.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { base } from '$app/paths';
+    export let url = "";
+    export let text = "";
+</script>
+
+<div>
+    <a href={url} >
+        <img src="{base}/ppt.svg" class="logo coa" alt="COA PDF" /> 
+    </a>
+    <p>{text}</p>
+</div>
+
+<style> 
+    .logo.coa:hover {
+      filter: drop-shadow(0 0 2em #FFCA26);
+    }
+</style>
