18b1f4b81970bfb8a43100531a77f874eff5a9ea
ninetynin <wekandmusic@gmail.com>
2022-11-07 12:10:33 +0530

    migrating to python and jsn

--- src/routes/3rdsem/coa/latest-slides/+page.svelte
+++ src/routes/3rdsem/coa/latest-slides/+page.svelte
@@ -1,16 +1,13 @@
 <script>
     import Pdf from "$lib/Pdf.svelte";
     let link12 = "https://learnermanipal.sharepoint.com/sites/ManipalOSF/Shared%20Documents/General/Branchwise%20Course%20Material/Computer%20science%20(Core)/Year-2/III-%20Sem/Computer%20organisation%20and%20architecture/COA%202022%20Updated%20Slides/1.%20Session%201%20and%202.pdf?CT=1667328885216&OR=ItemsView";
-    let link34 = "https://learnermanipal.sharepoint.com/sites/ManipalOSF/Shared%20Documents/General/Branchwise%20Course%20Material/Computer%20science%20(Core)/Year-2/III-%20Sem/Computer%20organisation%20and%20architecture/COA%202022%20Updated%20Slides/2.%20Session%203%20and%204.pdf?CT=1667329086776&OR=ItemsView";
 </script>
-
 
   <h1>COA_LATEST_SLIDES</h1>
   <div class="row" style="text-align: center">
     <Pdf url={link12} text="1 & 2" />
   </div>
 
-  
   <style>
     .row {
       display: flex;
