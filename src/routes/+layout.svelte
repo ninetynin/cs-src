7cf4afbcda712ca02e782bb427eaec18ecae3e56
ninetynin <wekandmusic@gmail.com>
2022-11-07 10:15:44 +0530

    fixed +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  import "../app.css";
+</script>
+
+<main class="container">
+  <slot />
+  
+  <footer class="footer">
+    <p class="footerfont">OSF Prototype</p>
+  </footer>
+
+  <style>
+    .footerfont {
+    font-size: 0.8rem;
+    font-weight: 400;
+    color: #6c6c6d;
+  }
+
+  /*set the footer to bottom of the screen */
+  .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+  }
+  </style>
+</main>
