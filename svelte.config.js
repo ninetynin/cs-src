5036e55f82ebd5a50cf0759a20436fac00ee3503
ninetynin <wekandmusic@gmail.com>
2022-11-07 06:05:35 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,24 +8,9 @@ const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
     preprocess: preprocess(),
 
-    // kit: {
-    //     adapter: adapter()
-    // }
-
     kit: {
-        // adapter: adapter({
-        //     // default options are shown. On some platforms
-        //     // these options are set automatically — see below
-        //     pages: 'build',
-        //     assets: 'build',
-        //     fallback: null,
-        //     precompress: false,
-        //     strict: true
-        // })
         adapter: static_adapter({
             pages: 'build',
             assets: 'build',
@@ -35,8 +20,8 @@ const config = {
         })
     },
     paths: {
-        // base: "/cs-src"
-        base: dev ? "" : "/cs-src"
+        base: "/cs-src/build"
+            // base: dev ? "" : "/cs-src"
     },
     appdir: 'internal',
 };
