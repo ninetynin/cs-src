eefa048bae7cc8542daec2e5d437de3fc642c451
ninetynin <wekandmusic@gmail.com>
2022-11-07 17:19:17 +0530

    trying withput importing base now

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,6 @@ import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 // import { dev } from '$app/environment';
 import static_adapter from '@sveltejs/adapter-static';
-// import { assets } from '$app/paths';
-
-
-// const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -22,8 +18,6 @@ const config = {
     },
     paths: {
         base: "/cs-src",
-        // assets: "/cs-src/static"
-        // base: dev ? "" : "/cs-src"
     },
     appdir: 'internal',
 };
