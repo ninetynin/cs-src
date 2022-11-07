202fd9b5f03c785127bf0e54ecf74038ad7b8996
ninetynin <wekandmusic@gmail.com>
2022-11-07 17:06:18 +0530

    remove nav later maybe

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
+<div>
+  <nav>
+    <ul>
+      <li><a href="/3rdsem/coa">COA</a></li>
+      <li><a href="/3rdsem/dsa">DSA</a></li>
+      <li><a href="/3rdsem/dsd">DSD</a></li>
+      <li><a href="/3rdsem/oops">OOPS</a></li>
+      <li><a href="/3rdsem/maths">MATHS</a></li>
+    </ul>
+  </nav>
+</div>
