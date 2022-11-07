8f5fb5077a67201ae6049aaed0a4fe484b6e4223
ninetynin <wekandmusic@gmail.com>
2022-11-07 06:38:58 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,82 +1,18 @@
-<<<<<<< Updated upstream
-// // import adapter from '@sveltejs/adapter-auto';
-// // import preprocess from 'svelte-preprocess';
-
-// // /** @type {import('@sveltejs/kit').Config} */
-// // const config = {
-// //     // Consult https://github.com/sveltejs/svelte-preprocess
-// //     // for more information about preprocessors
-// //     // preprocess: preprocess(),
-// //     preprocess: preprocess[preprocess()],
-
-// //     kit: {
-// //         // adapter: adapter()
-// //         adapter: adapter({
-// //             pages: 'build',
-// //             assets: 'build',
-// //             fallback: null,
-// //         }),
-// //         // adapter: staticAdapter({
-// //         //     pages: 'build',
-// //         //     assets: 'build',
-// //         //     fallback: null,
-// //         // }),
-// //         target: '#svelte',
-// //     }
-// // };
-
-// // export default config;
-
-=======
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { dev } from '$app/environment';
 import static_adapter from '@sveltejs/adapter-static';
->>>>>>> Stashed changes
-
-// // svelte.config.js
-// import adapter from '@sveltejs/adapter-static';
-// import static_adapter from '@sveltejs/adapter-static';
-
-// export default {
-//     kit: {
-//         // adapter: adapter({
-//         //     // default options are shown. On some platforms
-//         //     // these options are set automatically — see below
-//         //     pages: 'build',
-//         //     assets: 'build',
-//         //     fallback: null,
-//         //     precompress: false,
-//         //     strict: true
-//         // })
-//         adapter: static_adapter({
-//             pages: 'build',
-//             assets: 'build',
-//             fallback: null,
-//             precompress: false,
-//             strict: true
-//         }),
-//         paths: {
-//             base: '/CS-SRC',
-//             // assets: '/CS-SRC/static'
-//         }
-//     }
-// };
+import { assets } from '$app/paths';
 
 
-// -- https://github.com/sveltejs/kit/blob/7d985a152d865c28352da46a63f959d881c1a094/packages/adapter-static/README.md#github-pages
-// svelte.config.js
+const dev = process.argv.includes('dev');
 
-import { dev } from '$app/environment';
-import adapter from '@sveltejs/adapter-static';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: preprocess(),
 
-const dev = process.argv.includes('dev')
-
-export default {
     kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
+        adapter: static_adapter({
             pages: 'build',
             assets: 'build',
             fallback: null,
@@ -85,18 +21,11 @@ export default {
         })
     },
     paths: {
-<<<<<<< Updated upstream
-        base: dev ? '' : '/cs-src',
-        assets: dev ? '' : '/cs-src/static'
+        base: "/cs-src",
+        // assets: "/cs-src/static"
+        // base: dev ? "" : "/cs-src"
     },
-    appdir: 'internal',
-};
-=======
-        base: "/cs-src"
-            // base: dev ? "" : "/cs-src"
-    },
-    appdir: 'internal',
+    // appdir: 'internal',
 };
 
 export default config;
->>>>>>> Stashed changes
